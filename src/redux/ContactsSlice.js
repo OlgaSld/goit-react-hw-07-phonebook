1d21a05b68f7ccef8c25a4648c5de7abadcc71fc
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2023-10-05 14:10:17 +0300

    for check

--- src/redux/ContactsSlice.js
+++ src/redux/ContactsSlice.js
@@ -37,7 +37,7 @@ const contactsSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      state.items = state.items.filter(item => item.id !== action.payload);
+      state.items = state.items.filter(item => item.id !== action.payload.id);
     },
     [deleteContact.rejected]: handleRejected,
   },
